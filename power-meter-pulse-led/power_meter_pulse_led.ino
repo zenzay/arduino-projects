15297bb0987795b123843bbf63d5fe0a16cc4d93
Jens Hansen <31970501+zenzay@users.noreply.github.com>
2023-03-04 22:57:35 +0100

    Update power_meter_pulse_led.ino

--- power-meter-pulse-led/power_meter_pulse_led.ino
+++ power-meter-pulse-led/power_meter_pulse_led.ino
@@ -1,28 +1,28 @@
-// pulse_led - simulate the pulse led on a power panel
-const float pulse_rate = 1000.0f;        // number of blinks per kWh
-const float watt_target = 1000.0f;       // power consumption to simulate - not used if random_power flag is set
-const int led_pin = 16;                  // gpio pin number of led
-const uint32_t led_on_ms = 100;          // led on duration
-uint32_t blink_interval = ( 1000.0f / pulse_rate ) * ( 1000.0f / watt_target ) * 3600.0f;             // blink interval in milliseconds
-uint32_t now = 0;                        // current millis
-uint32_t last_blink = 0;                 // last blink millis
-int led_state = LOW;                     // initial led state
+// power_meter_pulse_led - simulate the pulse led on a power panel
+const int led_pin = 16;                 // gpio number of led pin
+const float pulse_rate = 1000.0;        // number of pulses per kWh
+const float power_use = 1000.0;         // watt. power consumption to simulate
+const int pulse_duration = 100;         // how many ms before we turn off LED again
+
+uint32_t now;
+uint32_t last_pulse;
+uint32_t pulse_interval; 
+int led_state = LOW;
 
 void setup() {
-  pinMode(led_pin, OUTPUT);                                 // blink!
-  last_blink = millis();                                    // get the base millis for evaluating the time spend since last blink
+  pinMode(led_pin, OUTPUT);
+  pulse_interval = ( 60 * 60 * 1000 / power_use ) * (pulse_rate / 1000); 
+  last_pulse = millis();
 }
 
 void loop() {
   now = millis();
-  if ( (now - last_blink) > blink_interval) {
-    // if millis passed since last blink is over the specified interval, turn led on
+  if ( (now - last_pulse) >= pulse_interval) {
     led_state = !led_state;
     digitalWrite(led_pin, led_state);
-    last_blink = now;                                  // remember when we last turn the led on
+    last_pulse = now;
   } else if (led_state == HIGH) {
-    if ( (now - last_blink) >= led_on_ms) {
-      // if led is on and the time is up, turn it off
+    if ( (now - last_pulse) >= pulse_duration) {
       led_state = !led_state;
       digitalWrite(led_pin, led_state);
     }
