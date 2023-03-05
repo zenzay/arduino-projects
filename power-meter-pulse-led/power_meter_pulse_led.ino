a30be5a5e22a78a4f962337f9f0ea7c25c4f9c12
Jens Hansen <31970501+zenzay@users.noreply.github.com>
2023-03-05 14:42:25 +0100

    Update power_meter_pulse_led.ino

--- power-meter-pulse-led/power_meter_pulse_led.ino
+++ power-meter-pulse-led/power_meter_pulse_led.ino
@@ -1,17 +1,24 @@
 // power_meter_pulse_led - simulate the pulse led on a power panel
-const int led_pin = 16;                 // gpio number of led pin
-const float pulse_rate = 1000.0;        // number of pulses per kWh
-const float power_use = 1000.0;         // watt. power consumption to simulate
-const int pulse_duration = 100;         // how many ms before we turn off LED again
 
-uint32_t now;
-uint32_t last_pulse;
-uint32_t pulse_interval; 
+const int led_pin = 16;
+const int pulse_duration = 100;
+const float power_consumption = 1000;
+const float pulse_rate = 1000;
+const float hour_ms = 3600000;
+
+unsigned long now;
+unsigned long last_pulse;
+unsigned long pulse_interval; 
 int led_state = LOW;
+
+unsigned long get_pulse_interval(float watt, float rate)
+{
+  return ( hour_ms / watt ) * (rate / 1000.0); 
+}
 
 void setup() {
   pinMode(led_pin, OUTPUT);
-  pulse_interval = ( 60 * 60 * 1000 / power_use ) * (pulse_rate / 1000); 
+  pulse_interval = get_pulse_interval(power_consumption, pulse_rate);
   last_pulse = millis();
 }
 
