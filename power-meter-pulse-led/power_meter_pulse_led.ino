dea513f09a4bbce4fe04fce8f274817432022c5b
Jens Hansen <31970501+zenzay@users.noreply.github.com>
2023-03-06 10:16:15 +0100

    Update power_meter_pulse_led.ino

--- power-meter-pulse-led/power_meter_pulse_led.ino
+++ power-meter-pulse-led/power_meter_pulse_led.ino
@@ -13,7 +13,7 @@ int led_state = LOW;
 void setup() {
   pinMode(led_pin, OUTPUT);
   noInterrupts();
-  pulse_interval = ( pulse_rate / 1000.0 ) * ( 1000.0 / power_comsumption ) * 3600.0;
+  pulse_interval = ( pulse_rate / 1000.0 ) * ( 1000.0 / power_consumption ) * 3600.0;
   last_pulse = millis();
 }
 
