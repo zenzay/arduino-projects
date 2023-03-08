9f519d521d691376849a639eb3b6cc2aafea11c2
Jens Hansen <31970501+zenzay@users.noreply.github.com>
2023-03-08 20:22:55 +0100

    Update pulse_meter.ino

--- pulse_meter/pulse_meter.ino
+++ pulse_meter/pulse_meter.ino
@@ -1,7 +1,7 @@
 /*
   Pulse Meter
 
-  Some Meters have a LED that pulses in relation to your power consumption.
+  Some Meters has a LED that pulses in relation to your power consumption.
   These LEDs usually have a pulse rate of a 1.000 pulses for every kWh consumed.
 
   This sketch tries to measure the pulses and report power consumption
